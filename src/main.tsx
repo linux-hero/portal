7e94b90d15605eb67eb047a261c22d8beb98c94a
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-25 23:17:09 -0300

    feat: create app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import {createRoot} from "react-dom/client";
+import App from "./app";
 
 function bootstrap() {
     const root = document.getElementById('root');
     if (!root)
         throw new Error("Root element not Found!");
 
-    createRoot(root).render(<h1>Hello World!</h1>);
+    createRoot(root).render(<App />)
 }
 
 bootstrap()
