b4df0d575f943122ee61e9fab0096429a40f5c4c
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-26 20:16:19 -0300

    test: modules.app

--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,11 +1,11 @@
 import {describe, expect, test} from "vitest";
 import {render, screen} from "@testing-library/react";
 import App from "../src/modules/app/main";
+import 'canvas';
 
 describe("root component", () => {
     test("should render hello world", () => {
        render(<App />);
-       screen.debug();
-       expect(screen.getByText("Olá, Mundo!")).toBeInTheDocument();
+       expect(screen.getByTestId("terminal")).toBeInTheDocument();
     });
 });
--- tests/hooks/setup.tsx
+++ tests/hooks/setup.tsx
@@ -1,4 +1,4 @@
-import {afterEach, expect} from "vitest";
+import {afterEach, expect, vi} from "vitest";
 import * as matchers from '@testing-library/jest-dom/matchers';
 import {cleanup} from "@testing-library/react";
 import '../../src/translation'
@@ -8,3 +8,18 @@ expect.extend(matchers);
 afterEach(() => {
     cleanup();
 })
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(),
+        removeListener: vi.fn(),
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
