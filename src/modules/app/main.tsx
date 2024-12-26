f97afa286c70b4c633ede910b8e7682e1a0da5dd
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-26 02:02:01 -0300

    feat: remove translation from app module

--- src/modules/app/main.tsx
+++ src/modules/app/main.tsx
@@ -1,11 +1,7 @@
-import {useTranslation} from "react-i18next";
 import Terminal from "../../components/terminal/terminal";
 
 export default function App() {
-    const { t } = useTranslation("modules::app");
-
     return <>
-        <h1>{t("welcome_message")}</h1>
         <Terminal />
     </>
 }
