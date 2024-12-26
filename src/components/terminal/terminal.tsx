d3e91d3f93ddc13c064a2e6991f366bdcc4e5d56
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-26 19:45:29 -0300

    feat: create terminal element

--- src/components/terminal/terminal.tsx
+++ src/components/terminal/terminal.tsx
@@ -1,6 +1,39 @@
-import {useTranslation} from "react-i18next";
+import {useEffect} from "react";
+import { Terminal as XTermTerminal } from '@xterm/xterm';
+import '@xterm/xterm/css/xterm.css';
 
 export default function Terminal() {
-    const { t } = useTranslation('components::terminal')
-    return <h1>{t("welcome_message")}</h1>
+    const terminalId = "terminal";
+
+    function openTerminal() {
+        const terminal = new XTermTerminal();
+        const terminalElement = document.getElementById(terminalId);
+        if (!terminalElement)
+            throw new Error("Unable to find terminal element!");
+
+        terminal.open(terminalElement);
+        return (terminal);
+    }
+
+    function writeToTerminal(terminal: XTermTerminal) {
+        const transformationTable: Record<number, string> = {
+            127: '\b \b',
+        };
+
+        terminal.onData((data) => {
+            data = data
+                .split('')
+                .map(char => transformationTable[char.charCodeAt(0)] || char)
+                .join();
+
+            terminal.write(data);
+        });
+    }
+
+    useEffect(() => {
+        const terminal = openTerminal();
+        writeToTerminal(terminal);
+    }, []);
+
+    return <div id={terminalId} data-testid={terminalId}></div>
 }
