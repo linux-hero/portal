1408887bdf5e93b9a2595af9c517b9d2c1f3b33b
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-28 00:55:41 -0300

    feat: install cheerpx

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,22 @@ import {defineConfig} from "vite";
 import viteReact from "@vitejs/plugin-react";
 import viteYaml from "@modyfi/vite-plugin-yaml";
 
+const viteServerConfig = () => ({
+    name: "add-headers",
+    configureServer: (server) => {
+        server.middlewares.use((req, res, next) => {
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            next();
+        });
+    },
+});
+
 export default defineConfig({
     plugins: [
         viteReact(),
-        viteYaml()
+        viteYaml(),
+        viteServerConfig()
     ],
     test: {
         globals: 'true',
@@ -16,5 +28,10 @@ export default defineConfig({
             include: ['src/**'],
             extension: ['ts', 'tsx']
         }
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "es2022"
+        }
     }
-})
+});
