e763f52c3ec51e891ff4bc1c8b5138324ab375d5
Letícia Aguiar <letsaguiar@yandex.com>
2024-12-28 01:24:31 -0300

    feat: use cheerpx engine behind xtermjs

--- src/components/terminal/handlers.ts
+++ src/components/terminal/handlers.ts
@@ -4,54 +4,27 @@ export interface TerminalOnKeyHandlerParams {
     terminal: Terminal;
     key: string;
     input: string;
+    sender: EventListener;
 }
 
 export type TerminalOnKeyHandler = (params: TerminalOnKeyHandlerParams) => string;
 
-export const defaultHandler: TerminalOnKeyHandler = ({ terminal, key, input }) => {
-    terminal.write(key);
+export const defaultHandler: TerminalOnKeyHandler = ({ key, input, sender }) => {
+    sender(key.charCodeAt(0) as any);
     return input + key;
 };
-
-export const deleteHandler: TerminalOnKeyHandler = ({ terminal, input }) => {
-    if (input.length) {
-        terminal.write('\b \b');
-        return input.slice(0, -1);
-    }
-
-    return input;
-}
-
-export const newLineHandler: TerminalOnKeyHandler = ({ terminal }) => {
-    terminal.write('\r\n');
-    return "";
-}
-
-export const tabHandler: TerminalOnKeyHandler = ({ terminal, input }) => {
-    terminal.write("    ");
-    return input + "    ";
-}
-
-export const promptHandler: TerminalOnKeyHandler = ({ terminal }) => {
-    terminal.write("$ ");
-    return "";
-}
 
 export const commandHandler: TerminalOnKeyHandler = ({ input }) => {
     console.log('command: ' + input);
     return input;
 }
 
-export const clearHandler: TerminalOnKeyHandler = ({ terminal }) => {
-    terminal.clear();
+export const clearHandler: TerminalOnKeyHandler = () => {
     return "";
 }
 
 export const defaultHandlers: Record<string, TerminalOnKeyHandler[]> = {
-    '\x09': [tabHandler],
-    '\x0c': [clearHandler],
-    '\x0d': [commandHandler, newLineHandler, promptHandler],
-    '\x7f': [deleteHandler],
+    '\x0d': [commandHandler, defaultHandler, clearHandler],
     'default': [defaultHandler],
 }
 
